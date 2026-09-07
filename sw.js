8f231b2b2ebaae9c50309b76b918b52cd4b23723
grich295 <gp-richardson76@hotmail.co.uk>
2026-09-07 22:22:38 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='safety-tracker-v1-0-4';
-const CORE=['./','./index.html','./styles-v104.css','./app-v104.js','./config.js','./manifest.webmanifest'];
+const CACHE='safety-tracker-v1-1';
+const CORE=['./','./index.html','./styles-v110.css','./app-v110.js','./config.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
